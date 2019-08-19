6147c3e9223a896956e79bce1a042b2b6f502ba8
v.monasteretskyy <v.monasteretskyy@gmail.com>
2019-08-19 16:06:42 +0300

    new page

--- js/common.js
+++ js/common.js
@@ -5,8 +5,8 @@ $(document).ready(function () {
         slidesToScroll: 1,
         nextArrow: '<button type="button" class="slick-next"></button>',
         prevArrow: '<button type="button" class="slick-prev"></button>',
-        // autoplay: true,
-        // autoplaySpeed: 2000,
+        autoplay: true,
+        autoplaySpeed: 2000,
         responsive: [
             {
                 breakpoint: 992,
@@ -49,5 +49,49 @@ $(document).ready(function () {
     $('main').click(function () {
         $('.header__search-field').hide();
     });
+
+    // duo slider
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        arrows: true,
+        dots: false,
+        // centerMode: true,
+        focusOnSelect: true,
+        nextArrow: '<button type="button" class="slick-next duo-arrow"></button>',
+        prevArrow: '<button type="button" class="slick-prev duo-arrow""></button>',
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 375,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+
 
 });
